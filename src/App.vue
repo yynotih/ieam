d739288ca3a7c57f5b6e1d87c3dca9fc35a6e270
yynotih <yynotih@protonmail.com>
2017-09-27 17:33:34 +0800

    ieam init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <ol>
+      <li>
+        <router-link to='/hello'>to hello</router-link><br/>
+      </li>
+      <li>
+        <router-link to='/test'> to test</router-link><br/>
+      </li>
+    </ol>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id='Test'>
+        <span :title='title'>{{test_greet}}</span>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'Test',
+    data(){
+        return {
+            title: 'GOOD LUCK!',
+            test_greet: 'Welcome to my test_page'
+        } 
+    }
+}
+</script>
+
+<style>
+    
+</style>
